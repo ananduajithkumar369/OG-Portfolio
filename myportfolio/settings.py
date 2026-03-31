ce7e1cf4ba28238086247835872a996be1d16a71
anandu <ananduajithkumar369@gmail.com>
2026-03-31 11:12:59 +0530

    debug issue  solved

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-3a_+nm++m@^ifznkb*&4((rw5&25e3t$sdf#va9a)6kv5gdjm$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+print("DEBUG ALLOWED_HOSTS:", ALLOWED_HOSTS)
 
 
 # Application definition
